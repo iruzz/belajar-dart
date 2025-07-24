d837eb397547b07e1b68b8d8e3e990f24fd34ae8
iruzz <rabbaniruzz45@gmail.com>
2025-07-24 22:45:12 +0700

    mixin

--- test.dart
+++ test.dart
@@ -8,20 +8,26 @@ tambah(angka1, angka2) {
 //   print("Nama saya $nama, umur saya $umur tahun");
 // }
 
-class kemampuan {
+mixin Kemampuan {
+  void berlari(jarak, waktu) {
+    print("Saya bisa berlari sejauh $jarak meter dalam waktu $waktu detik");
+  }
+}
+
+class mamalia {
   void menyusui() {
     print("Saya bisa menyusui");
   }
 }
 
-class Orang extends kemampuan {
+class Orang extends mamalia with Kemampuan {
   String nama;
   int umur;
 
   // Constructor
   // Orang(this.nama, this.umur);
 
-  Orang({ required this.nama, required this.umur});
+  Orang({required this.nama, required this.umur});
 
   void perkenalan() {
     print("Nama saya $nama, umur saya $umur tahun");
@@ -30,9 +36,9 @@ class Orang extends kemampuan {
 
 void main() {
   print("hellow world");
-  Orang orang1 = Orang(nama: "Ruzz", umur: 16);
+  Orang orang1 = Orang(nama: "Fairuz", umur: 16);
   orang1.perkenalan();
   orang1.menyusui();
-
+  orang1.berlari(100, 10);
   // print(tambah(2, 5));
 }
