13824ce97feff159c2c991a482476675883d1c2a
iruzz <rabbaniruzz45@gmail.com>
2025-07-24 21:47:45 +0700

    Generative Constructor

--- test.dart
+++ test.dart
@@ -4,12 +4,26 @@ tambah(angka1, angka2) {
   return hasil;
 }
 
-orang(nama, umur) {
-  print("Nama saya $nama, umur saya $umur tahun");
+// orang(nama, umur) {
+//   print("Nama saya $nama, umur saya $umur tahun");
+// }
+
+class Orang {
+  String nama;
+  int umur;
+
+  // Constructor
+  Orang(this.nama, this.umur);
+
+  void perkenalan() {
+    print("Nama saya $nama, umur saya $umur tahun");
+  }
 }
 
 void main() {
   print("hellow world");
-  orang("Budi", 20);
+  Orang orang1 = Orang("Budi", 20);
+  print(orang1.nama);
+
   // print(tambah(2, 5));
 }
