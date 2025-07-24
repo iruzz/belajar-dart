43972bee8859b4a2ab77723e2d36eba3ca2757ca
iruzz <rabbaniruzz45@gmail.com>
2025-07-24 20:51:02 +0700

    call functions

---
+++ test.dart
@@ -0,0 +1,15 @@
+tambah(angka1, angka2) {
+  int hasil;
+  hasil = angka1 + angka2;
+  return hasil;
+}
+
+orang(nama, umur) {
+  print("Nama saya $nama, umur saya $umur tahun");
+}
+
+void main() {
+  print("hellow world");
+  orang("Budi", 20);
+  // print(tambah(2, 5));
+}
