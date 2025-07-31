4e491fb976c29466e2ed0fca6222b3e4469de0c2
iruzz <rabbaniruzz45@gmail.com>
2025-07-31 11:40:26 +0700

    update rehan

--- reyhan.dart
+++ reyhan.dart
@@ -1,12 +1,20 @@
 void main() {
-  String name = 'Reyhan';
-  String hobby = "Berenang";
-  String jenis_kelamin = '''Laki''';
-  print(name.length);
-  print(hobby.contains('Berenang'));
-  print(name.toLowerCase());
-  print(name.toUpperCase());
-  print(jenis_kelamin.startsWith('Laki'));
-  print(jenis_kelamin.endsWith('Laki'));
-  print(name.toLowerCase().replaceAll('r', 'n'));
+  String name = 'Reyhan adipramana nugraha';
+  String hobby = "Bermain basket";
+
+  print("String Length : ${name.length}");
+  print("String Contains : ${name.contains("Reyhan")}");
+  print("String in Upper Case : ${name.toUpperCase()}");
+  print("String in Lower Case : ${name.toLowerCase()}");
+  print("Start With : ${name.startsWith("R")}");
+  print("End with : ${name.endsWith("a")}");
+  print("Substring (1, 7) : ${name.substring(1, 7)}");
+  print("String with Trim : ${name.trim()}");
+  print("String with Trim Left : ${name.trimLeft()}");
+  print("String with Trim Right : ${name.trimRight()}");
+  print("String with isEmpty : ${name.isEmpty}");
+  print("String with isNotEmpty : ${name.isNotEmpty}");
+  print("String Splited by Space : ${name.split(" ")}");
+  print("String with ReplaceAll : ${name.toLowerCase().replaceAll("r", "r")}");
+  print("String with Compare : ${name.compareTo(hobby)}");
 }
