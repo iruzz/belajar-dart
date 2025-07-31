15f4f86c63c70e3c4e948813050bdb032f9e9527
iruzz <rabbaniruzz45@gmail.com>
2025-07-31 11:56:10 +0700

    rehan final

--- reyhan.dart
+++ reyhan.dart
@@ -7,9 +7,14 @@ void main() {
   print("String in Upper Case : ${name.toUpperCase()}");
   print("String in Lower Case : ${name.toLowerCase()}");
   print("Start With : ${name.startsWith("R")}");
-  print("Start with : ${name.endsWith("a")}");
-  print("Substring (1, 6) : ${name.substring(1, 6)}");
+  print("End with : ${name.endsWith("a")}");
+  print("Subsng (1, 7) : ${name.substring(1, 7)}");
   print("String with Trim : ${name.trim()}");
   print("String with Trim Left : ${name.trimLeft()}");
   print("String with Trim Right : ${name.trimRight()}");
+  print("String with isEmpty : ${name.isEmpty}");
+  print("String with isNotEmpty : ${name.isNotEmpty}");
+  print("String Splited by Space : ${name.split(" ")}");
+  print("String with ReplaceAll : ${name.toLowerCase().replaceAll("r", "r")}");
+  print("String with Compare : ${name.compareTo(hobby)}");
 }
