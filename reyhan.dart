d0d912d91567c94a97e6fd222e4e9f6523938eb3
iruzz <rabbaniruzz45@gmail.com>
2025-07-31 11:55:44 +0700

    update reyhan

--- reyhan.dart
+++ reyhan.dart
@@ -8,5 +8,8 @@ void main() {
   print("String in Lower Case : ${name.toLowerCase()}");
   print("Start With : ${name.startsWith("R")}");
   print("Start with : ${name.endsWith("a")}");
-  
+  print("Substring (1, 6) : ${name.substring(1, 6)}");
+  print("String with Trim : ${name.trim()}");
+  print("String with Trim Left : ${name.trimLeft()}");
+  print("String with Trim Right : ${name.trimRight()}");
 }
